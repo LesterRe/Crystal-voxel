b4e7bccb432b09f4a3c220495c5ab78339095b4a
Dongxu Li <WangRong089@gmail.com>
2022-07-16 11:46:13 +1000

    Norm

--- main.py
+++ main.py
@@ -23,12 +23,12 @@ parser.add_argument('--epochs', default=30, type=int, metavar='N',
                     help='number of total epochs to run (default: 30)')
 parser.add_argument('--disable-cuda', action='store_true',
                     help='Disable CUDA')
-parser.add_argument('--train-ratio', default=0.8, type=float, metavar='N',
+parser.add_argument('--train-ratio', default=0.6, type=float, metavar='N',
                     help='percentage of training data to be loaded (default 0.8)')
-parser.add_argument('--val-ratio', default=0.1, type=float, metavar='N',
+parser.add_argument('--val-ratio', default=0.2, type=float, metavar='N',
                     help='percentage of validation data to be loaded (default '
                          '0.1)')
-parser.add_argument('--test-ratio', default=0.1, type=float, metavar='N',
+parser.add_argument('--test-ratio', default=0.2, type=float, metavar='N',
                     help='percentage of test data to be loaded (default 0.1)')
 
 
@@ -39,11 +39,12 @@ best_mae_error = 1e10
 
 def main():
     global args, best_mae_error
+	
+    model = generate_model(18)
     
     if args.cuda:
         model.cuda()
     
-    model = generate_model(18)
 
     dataset = Voxel_Data(args.root_dir)
 
@@ -57,39 +58,42 @@ def main():
             pin_memory=args.cuda,
             return_test=True)
 
-
+    sample_data_list = [dataset[i] for i in
+                                sample(range(len(dataset)), 500)]
+    _, sample_target, _ = collate_pool(sample_data_list)
+    normalizer = Normalizer(sample_target)
 
     criterion = nn.L1Loss()
 
-    optimizer = optim.Adam(model.parameters(), 0.01,
+    optimizer = optim.Adam(model.parameters(), 0.001,
                                    weight_decay=0)
 
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
 
-    for epoch in range(100):
+    for epoch in range(args.epochs):
         # train for one epoch
-        train_epoch(epoch, train_loader, model, criterion, optimizer)
+        train_epoch(epoch, train_loader, model, criterion, optimizer, normalizer)
 
         # evaluate on validation set
-        mae_error = validate(val_loader, model, criterion)
+        mae_error = validate(val_loader, model, criterion, normalizer)
 
-        scheduler.step()
+       # scheduler.step()
 
-        if mae_error < best_mae_error:
-            best_mae_error = min(mae_error, best_mae_error)
-            torch.save({
-                'epoch': epoch + 1,
-                'state_dict': model.state_dict(),
-                'best_mae_error': best_mae_error,
-                'optimizer': optimizer.state_dict()
-            }, 'model_best_resnet.pth.tar')
+       # if mae_error < best_mae_error:
+       #     best_mae_error = min(mae_error, best_mae_error)
+       #     torch.save({
+       #         'epoch': epoch + 1,
+       #         'state_dict': model.state_dict(),
+       #         'best_mae_error': best_mae_error,
+       #         'optimizer': optimizer.state_dict()
+       #     }, 'model_best_resnet.pth.tar')
 
 
         # test best model
-        print('---------Evaluate Model on Test Set---------------')
-        best_checkpoint = torch.load('model_best_resnet.pth.tar')
-        model.load_state_dict(best_checkpoint['state_dict'])
-        validate(test_loader, model, criterion)
+      #  print('---------Evaluate Model on Test Set---------------')
+      #  best_checkpoint = torch.load('model_best_resnet.pth.tar')
+      #  model.load_state_dict(best_checkpoint['state_dict'])
+        validate(test_loader, model, criterion, normalizer)
 
 
 class AverageMeter(object):
@@ -111,7 +115,7 @@ class AverageMeter(object):
         self.avg = self.sum / self.count
         
 
-def train_epoch(epoch, data_loader, model, criterion, optimizer):
+def train_epoch(epoch, data_loader, model, criterion, optimizer, normalizer):
     print('train at epoch {}'.format(epoch))
 
     model.train()
@@ -123,19 +127,19 @@ def train_epoch(epoch, data_loader, model, criterion, optimizer):
     end_time = time.time()
     for i, (inputs, targets) in enumerate(data_loader):
         data_time.update(time.time() - end_time)
-        
-        if args.cuda:
-            input_var = Variable(inputs.cuda(non_blocking=True))
-            targets = Variable(targets.cuda(non_blocking=True))
-        else:
-            input_var = Variable(inputs)
-            targets = Variable(targets)
-
         inputs = inputs.transpose(1,4).float()
         targets = targets.float()
+        targets_norm = normalizer.norm(targets)
+        if args.cuda:
+            input_var = Variable(inputs.cuda(non_blocking=True))
+            targets_var = Variable(targets_norm.cuda(non_blocking=True))
+        else:
+            input_var = Variable(inputs)
+            targets_var = Variable(targets_norm)
+
         
-        outputs = model(inputs)
-        loss = criterion(outputs, targets)
+        outputs = model(input_var)
+        loss = criterion(outputs, targets_var)
                 
         losses.update(loss.data, inputs.size(0))
         
@@ -173,7 +177,7 @@ def train_epoch(epoch, data_loader, model, criterion, optimizer):
     #    torch.save(states, save_file_path)    
     
     
-def validate(data_loader, model, criterion):
+def validate(data_loader, model, criterion, normalizer):
     
     model.eval()
 
@@ -184,18 +188,18 @@ def validate(data_loader, model, criterion):
     end_time = time.time()
     for i, (inputs, targets) in enumerate(data_loader):
         data_time.update(time.time() - end_time)
-        
+        inputs = inputs.transpose(1,4).float() 
+        targets = targets.float()
+        target_normed = normalizer.norm(targets)
         if args.cuda:
             input_var = Variable(inputs.cuda(non_blocking=True))
-            targets = Variable(targets.cuda(non_blocking=True))
+            targets_var = Variable(target_normed.cuda(non_blocking=True))
         else:
-            input_var = Variable(inputs)
-            targets = Variable(targets)
+            input_var = Variable(input_var)
+            targets_var = Variable(target_normed)
 
-        inputs = inputs.transpose(1,4).float()
-        targets = targets.float()
-        outputs = model(inputs)
-        loss = criterion(outputs, targets)
+        outputs = model(input_var)
+        loss = criterion(outputs, targets_var)
                 
         losses.update(loss.data, inputs.size(0))
         
